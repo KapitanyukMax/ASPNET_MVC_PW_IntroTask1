166839afa8bc9627aee68c7ae434703987312066
KapitanyukMax <maxik20192006max@gmail.com>
2023-09-09 11:30:18 +0300

    Add project files.

---
+++ ASPNET_MVC_PW_IntroTask1/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using ASPNET_MVC_PW_IntroTask1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASPNET_MVC_PW_IntroTask1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Model()
+        {
+            return View();
+        }
+
+        public IActionResult View()
+        {
+            return base.View();
+        }
+
+        public IActionResult Controller()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASPNET_MVC_PW_IntroTask1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPNET_MVC_PW_IntroTask1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
